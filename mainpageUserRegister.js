ba7c91563c9422d37bbd768b2f88ffa9867a04df
Mosieiko_Kaladin <dantesdwanye@gmail.com>
2026-04-22 18:09:14 +0300

    Delete mainpageUserRegister.js

--- mainpageUserRegister.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const username = localStorage.getItem("regUsername");
-
-    if (username) {
-        document.getElementById("openProfile").textContent = username;
-    }
-});
